ad0ea3da1f636e5b6e41c7dabc7475d8dc83caea
Psycomentis06 <alibenamor30@gmail.com>
2021-12-19 20:47:14 +0100

    Cli generated

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout/layout.component';
 import { RouterModule } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 
 
 @NgModule({
   declarations: [
-    LayoutComponent
+    LayoutComponent,
+    NavbarComponent
   ],
   imports: [
     CommonModule,
