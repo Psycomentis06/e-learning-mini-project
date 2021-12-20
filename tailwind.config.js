2554b5cdad3c7667a8a66e01f08dfe04bdd1413a
Psycomentis06 <alibenamor30@gmail.com>
2021-12-20 11:53:56 +0100

    Skin color setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
+const skinColorOpacity = ({ opacityValue }, varName) => {
+  if (opacityValue !== undefined) return `rgba(var(${varName}, ${opacityValue}))`;
+  return `rgb(var(${varName}))`;
+}
+
 module.exports = {
   content: ["./src/**/*.{html,js,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      textColor: {
+        skin: {
+          
+        }
+      }
+    },
   },
   plugins: [],
 };
