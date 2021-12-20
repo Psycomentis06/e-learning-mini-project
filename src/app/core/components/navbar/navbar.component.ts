017990063763acae995e1a4ea238d184d5081acb
Psycomentis06 <alibenamor30@gmail.com>
2021-12-20 22:57:36 +0100

    FontAwesome icons

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import {faBars, faAngleLeft} from '@fortawesome/free-solid-svg-icons'
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor() {}
+  faBars = faBars;
+  faAngleLeft = faAngleLeft;
+  ngOnInit(): void {}
 }
