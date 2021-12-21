7360545477c4f05ca94c20cab7b4fa6c322803c0
Psycomentis06 <alibenamor30@gmail.com>
2021-12-21 23:56:34 +0100

    basic methods

--- src/app/core/service/navbar.service.ts
+++ src/app/core/service/navbar.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NavbarService {
+  private _navbarSubject: BehaviorSubject<boolean>;
+  readonly navbarSubject$: Observable<boolean>;
+  constructor() {
+    this._navbarSubject = new BehaviorSubject<boolean>(false);
+    this.navbarSubject$ = this._navbarSubject.asObservable();
+  }
 
-  constructor() { }
+  toggle() {
+    this._navbarSubject.next(!this._navbarSubject.getValue());
+  }
+
+  open(): void {
+    this._navbarSubject.next(true);
+  }
+
+  close():void {
+    this._navbarSubject.next(false);
+  }
 }
