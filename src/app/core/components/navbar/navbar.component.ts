c05432ab942a25c606df5057494a867831e8e644
Psycomentis06 <alibenamor30@gmail.com>
2021-12-22 11:39:58 +0100

    actions nav

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {faBars, faAngleLeft} from '@fortawesome/free-solid-svg-icons'
+import {faBars, faAngleLeft, faSearch, faBookmark, faUser, faPaintBrush} from '@fortawesome/free-solid-svg-icons'
 import { Observable } from 'rxjs';
 import { NavbarService } from '../../service/navbar.service';
 @Component({
@@ -8,8 +8,14 @@ import { NavbarService } from '../../service/navbar.service';
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-  faBars = faBars;
-  faAngleLeft = faAngleLeft;
+  fontAwesomeIcons = {
+    faBars,
+    faAngleLeft,
+    faSearch,
+    faBookmark,
+    faUser,
+    faPaintBrush
+  }
   
   navbarMenu: Observable<boolean>;
   constructor(public navbarService: NavbarService) {
