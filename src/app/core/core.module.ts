c61af23044e6e4bc5ecdff3547097df7e8543165
Psycomentis06 <alibenamor30@gmail.com>
2021-12-22 12:26:52 +0100

    Cli generated

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,14 +4,16 @@ import { LayoutComponent } from './layout/layout.component';
 import { RouterModule } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LogoComponent } from './components/logo/logo.component';
-import {FontAwesomeModule} from '@fortawesome/angular-fontawesome'
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import { SearchComponent } from './components/search/search.component'
 
 
 @NgModule({
   declarations: [
     LayoutComponent,
     NavbarComponent,
-    LogoComponent
+    LogoComponent,
+    SearchComponent
   ],
   imports: [
     CommonModule,
