a89289a42b3c8ed82ed0160a149a0c267432a2ee
Psycomentis06 <alibenamor30@gmail.com>
2021-12-22 23:43:26 +0100

    Search observables & states

--- src/app/core/service/search.service.ts
+++ src/app/core/service/search.service.ts
@@ -1,13 +1,33 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, debounce, debounceTime, distinctUntilChanged, interval, map, Observable, Subscription, tap } from 'rxjs';
+import { ISearchDataSource } from '../components/search/search-datasource.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
+  // Visibility states
   private $searchVisibilitySubject: BehaviorSubject<boolean>;
-  private  _searchVisibilityObservable: Observable<boolean>;
+  private _searchVisibilityObservable: Observable<boolean>;
+  
+  private searchDataSource: ISearchDataSource;
+  
+  // Search states
+  private $searchValueSubject: BehaviorSubject<string>;
+  private _searchValueObservable: Observable<string>;
+  private searchValueSubscription?: Subscription;
+
+  // DataSource states
+  private $searchDataSourceSubject: BehaviorSubject<ISearchDataSource>;
+  private _searchDataSourceObservable: Observable<ISearchDataSource>;
+  private searchDataSourceSubscription?: Subscription;
+
   constructor() {
+    this.searchDataSource = {}
+    this.$searchDataSourceSubject = new BehaviorSubject<ISearchDataSource>(this.searchDataSource);
+    this._searchDataSourceObservable = this.$searchDataSourceSubject.asObservable();
+    this.$searchValueSubject = new BehaviorSubject<string>("");
+    this._searchValueObservable = this.$searchValueSubject.asObservable();
     this.$searchVisibilitySubject = new BehaviorSubject<boolean>(false);
     this._searchVisibilityObservable = this.$searchVisibilitySubject.asObservable();
   }
@@ -28,5 +48,23 @@ export class SearchService {
   public get visible() : Observable<boolean> {
     return this._searchVisibilityObservable
   }
+
+
+  search(value: string): void {
+    this.$searchValueSubject.next(value);
+  }
   
+  searchSubscribe(): void {
+    this.searchValueSubscription = this._searchValueObservable.pipe(
+      debounceTime(1000),
+      distinctUntilChanged(),
+      tap((v) => v)
+    ).subscribe();
+  }
+
+  searchUnsubscribe(): void {
+    if (this.searchValueSubscription) this.searchValueSubscription.unsubscribe();
+  }
+ 
+
 }
