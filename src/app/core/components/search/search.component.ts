a3ee1c9a929119e6210d6169c9a836715531f384
Psycomentis06 <alibenamor30@gmail.com>
2021-12-22 16:26:45 +0100

    icons

--- src/app/core/components/search/search.component.ts
+++ src/app/core/components/search/search.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { faSearch, faTimes } from '@fortawesome/free-solid-svg-icons'
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
-
+  fontAwesomeIcons = {
+    faSearch,
+    faTimes
+  }
   constructor() { }
 
   ngOnInit(): void {
