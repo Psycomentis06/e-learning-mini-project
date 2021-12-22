9e07179c045776534b912eda5b57a0ab2530896c
Psycomentis06 <alibenamor30@gmail.com>
2021-12-22 18:15:50 +0100

    show | hide

--- src/app/core/service/search.service.ts
+++ src/app/core/service/search.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
+  private $searchVisibilitySubject: BehaviorSubject<boolean>;
+  readonly _searchVisibilityObservable: Observable<boolean>;
+  constructor() {
+    this.$searchVisibilitySubject = new BehaviorSubject<boolean>(false);
+    this._searchVisibilityObservable = this.$searchVisibilitySubject.asObservable();
+  }
 
-  constructor() { }
+  toggle() {
+    this.$searchVisibilitySubject.next(!this.$searchVisibilitySubject.getValue());
+  }
+
+  show() {
+    this.$searchVisibilitySubject.next(true);
+  }
+
+  hide() {
+    this.$searchVisibilitySubject.next(false);
+  }
 }
