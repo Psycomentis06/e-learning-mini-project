5238f95af04623cef374e4114e0873de8c88330e
Psycomentis06 <alibenamor30@gmail.com>
2021-12-25 23:52:12 +0100

    Search for actions & links

--- src/app/core/service/search.service.ts
+++ src/app/core/service/search.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, debounce, debounceTime, distinctUntilChanged, interval, map, Observable, Subscription, tap } from 'rxjs';
-import { ISearchDataSource } from '../components/search/search-datasource.interface';
+import { BehaviorSubject, debounceTime, distinctUntilChanged, Observable, Subscription, tap } from 'rxjs';
+import { ISearchDataSource, ISearchDataSourceAction, ISearchDataSourceLink } from './interface/search-datasource.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +9,10 @@ export class SearchService {
   // Visibility states
   private $searchVisibilitySubject: BehaviorSubject<boolean>;
   private _searchVisibilityObservable: Observable<boolean>;
-  
-  private searchDataSource: ISearchDataSource;
-  
+
+  private searchDataSourceOrigin: ISearchDataSource;
+  private searchDataSourceClone: ISearchDataSource;
+
   // Search states
   private $searchValueSubject: BehaviorSubject<string>;
   private _searchValueObservable: Observable<string>;
@@ -23,8 +24,119 @@ export class SearchService {
   private searchDataSourceSubscription?: Subscription;
 
   constructor() {
-    this.searchDataSource = {}
-    this.$searchDataSourceSubject = new BehaviorSubject<ISearchDataSource>(this.searchDataSource);
+    this.searchDataSourceOrigin = {
+      actions: [
+        {
+          name: 'Logout',
+          action: () => {
+            alert('Logout');
+          },
+          icon: 'fas fa-sign-out-alt',
+          tags: []
+        },
+        {
+          name: 'Change Theme',
+          action: () => {
+            alert('Change Theme');
+          },
+          icon: 'fas fa-paint-brush',
+          tags: []
+        },
+        {
+          name: 'Clear local storage',
+          action: () => {
+            alert('Clear cache');
+          },
+          icon: 'fas fa-trash',
+          tags: []
+        },
+        {
+          name: 'Remove bookmarks',
+          action: () => {
+            alert('Bookmarks');
+          },
+          icon: 'fas fa-bookmark',
+          tags: []
+        },
+        {
+          name: 'Change Accent Color',
+          action: () => {
+            alert('Accent Color');
+          },
+          icon: 'fas fa-palette',
+          tags: []
+        },
+        {
+          name: 'Send Report',
+          action: () => {
+            alert('Report');
+          },
+          icon: 'fas fa-sign-out-alt',
+          tags: []
+        }
+      ],
+
+      links: [
+        {
+          name: 'Home',
+          path: '',
+          breadcrumb: 'Home',
+          icon: 'fas fa-igloo',
+          tags: []
+        },
+        {
+          name: 'Change Password',
+          path: '/account/settings/password',
+          breadcrumb: 'Home > Account > Settings > Password',
+          icon: 'fas fa-igloo',
+          tags: []
+        },
+        {
+          name: 'Lessons',
+          path: '/lessons',
+          breadcrumb: 'Home > Lessons',
+          icon: 'fas fa-igloo',
+          tags: []
+        },
+        {
+          name: 'Pricing',
+          path: '/pricing',
+          breadcrumb: 'Home > Pricing',
+          icon: 'fas fa-igloo',
+          tags: []
+        },
+        {
+          name: 'About',
+          path: '/about',
+          breadcrumb: 'Home > About',
+          icon: 'fas fa-igloo',
+          tags: []
+        },
+        {
+          name: 'Account',
+          path: '/account',
+          breadcrumb: 'Home > Account',
+          icon: 'fas fa-igloo',
+          tags: []
+        },
+        {
+          name: 'Sign In',
+          path: '/signin',
+          breadcrumb: 'Home > Sign In',
+          icon: 'fas fa-igloo',
+          tags: []
+        },
+        {
+          name: 'Bookmarks',
+          path: '/bookmarks',
+          breadcrumb: 'Home > Bookmarks',
+          icon: 'fas fa-igloo',
+          tags: []
+        }
+      ],
+    };
+    this.searchDataSourceClone = this.searchDataSourceOrigin;
+    this.$searchDataSourceSubject = new BehaviorSubject<ISearchDataSource>(this.searchDataSourceClone);
     this._searchDataSourceObservable = this.$searchDataSourceSubject.asObservable();
     this.$searchValueSubject = new BehaviorSubject<string>("");
     this._searchValueObservable = this.$searchValueSubject.asObservable();
@@ -44,8 +156,8 @@ export class SearchService {
     this.$searchVisibilitySubject.next(false);
   }
 
-  
-  public get visible() : Observable<boolean> {
+
+  public get visible(): Observable<boolean> {
     return this._searchVisibilityObservable
   }
 
@@ -53,18 +165,55 @@ export class SearchService {
   search(value: string): void {
     this.$searchValueSubject.next(value);
   }
-  
+
   searchSubscribe(): void {
     this.searchValueSubscription = this._searchValueObservable.pipe(
       debounceTime(1000),
       distinctUntilChanged(),
-      tap((v) => v)
+      tap((v) => {
+        this.getData(v);
+        return v;
+      })
     ).subscribe();
   }
 
   searchUnsubscribe(): void {
     if (this.searchValueSubscription) this.searchValueSubscription.unsubscribe();
   }
- 
+
+
+  getData(value: string): void {
+    this.searchDataSourceClone = Object.assign({}, this.searchDataSourceOrigin)
+    // Filter links
+    if (this.searchDataSourceClone.links) {
+      let links: ISearchDataSourceLink[] | undefined = []
+      links = this.searchDataSourceOrigin.links?.filter(
+          (l) =>
+            l.name.trim().toLowerCase().includes(value.toLowerCase()) ||
+            l.path.trim().toLowerCase().includes(value.toLowerCase())
+        );
+      this.searchDataSourceClone.links = links
+    }
+
+    // Filter Actions
+    if (this.searchDataSourceClone.actions) {
+      let actions: ISearchDataSourceAction[] | undefined = []
+      actions = this.searchDataSourceClone.actions?.filter(
+        (a) =>
+          a.name.trim().toLowerCase().includes(value.trim().toLowerCase()) ||
+          value.length === 0
+      );
+      this.searchDataSourceClone.actions = actions
+    }
+
+    // Filter Courses
+    //this.searchDataSourceClone.courses?.filter(c => c.name)
+
+    this.$searchDataSourceSubject.next(this.searchDataSourceClone); 
+  }
+
+  $dataSource(): Observable<ISearchDataSource> {
+    return this._searchDataSourceObservable
+  }
 
 }
