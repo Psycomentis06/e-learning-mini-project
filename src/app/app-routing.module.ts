0f70a6b0245109df9cd858bfa12153bae1de2826
Psycomentis06 <alibenamor30@gmail.com>
2021-12-26 04:54:56 +0100

    Configure basic lazy loading HomeModule routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotfoundComponent } from './pages/errors/notfound/notfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./home/home.module').then( m => m.HomeModule)
+  },
+  {
+    path: 'notfound',
+    component: NotfoundComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'notfound'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NotfoundComponent } from './pages/errors/notfound/notfound.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NotfoundComponent,
   ],
   imports: [
     BrowserModule,
