3e1a8f820c62123d6187a4ea8772c6e2bf6b689f
Psycomentis06 <alibenamor30@gmail.com>
2022-01-03 20:39:59 +0100

    fontawesome module

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {faMinus} from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  fontAwesomeIcons = {
+    faMinus
+  }
   constructor() { }
 
   ngOnInit(): void {
