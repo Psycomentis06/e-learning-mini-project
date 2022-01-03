e305af15891fcf70dc64c383644921c104498b89
Psycomentis06 <alibenamor30@gmail.com>
2022-01-03 16:51:30 +0100

    UI

--- src/app/home/components/course-card/course-card.component.ts
+++ src/app/home/components/course-card/course-card.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, HostListener, Input, OnInit } from '@angular/core';
+import { IFramework } from 'src/app/interfaces/framework.interface';
+import { ILanguage } from 'src/app/interfaces/language.interface';
 
 @Component({
   selector: 'app-course-card',
@@ -9,17 +11,27 @@ export class CourseCardComponent implements OnInit {
   @Input('title') title: string;
   @Input('bg-color') bgColor: string;
   @Input('text-color') textColor: string;
-  @Input('class') class: string;
-  @Input('img-background') imgBackground: string;
+  @Input('description') description: string;
+
+   radialXTransition: string;
+   radialYTransition: string;
   constructor() {
-    this.title = "";
+    this.title = "No Title";
+    this.description = "No description"
     this.bgColor = "rgb(0,0,0)";
-    this.class = "";
-    this.imgBackground = "";
-    this.textColor = "rgb(255,255,255)"
+    this.textColor = "rgb(255,255,255)",
+
+    this.radialXTransition = "25%";
+    this.radialYTransition = "25%";
    }
 
   ngOnInit(): void {
+  }
+
+  @HostListener('mousemove', ['$event'])
+  mouseMoveHandler($event: MouseEvent) {    
+    this.radialXTransition = ($event.offsetX / 2) + "px";
+    this.radialYTransition = ($event.offsetY /2) + "px";
   }
 
 }
