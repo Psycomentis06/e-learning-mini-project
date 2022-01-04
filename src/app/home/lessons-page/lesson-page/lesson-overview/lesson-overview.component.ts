6f28f4582ab0b8f229c41937b9cbfeced3f29d6e
Psycomentis06 <alibenamor30@gmail.com>
2022-01-04 21:46:29 +0100

    1st

--- src/app/home/lessons-page/lesson-page/lesson-overview/lesson-overview.component.ts
+++ src/app/home/lessons-page/lesson-page/lesson-overview/lesson-overview.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { ICourse } from 'src/app/interfaces/course.interface';
+import { CourseService } from 'src/app/services/course.service';
 
 @Component({
   selector: 'app-lesson-overview',
   templateUrl: './lesson-overview.component.html',
-  styleUrls: ['./lesson-overview.component.scss']
+  styleUrls: ['./lesson-overview.component.scss'],
 })
 export class LessonOverviewComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  $courseObservable: Observable<ICourse>;
+  constructor(
+    private courseService: CourseService,
+    private activatedRoute: ActivatedRoute
+  ) {
+    this.$courseObservable = this.getCourse();
   }
 
+  ngOnInit(): void {}
+
+  getCourse(): Observable<ICourse> {
+    return this.courseService.get(
+      Number(this.activatedRoute.snapshot.paramMap.get('id'))
+    );
+  }
 }
