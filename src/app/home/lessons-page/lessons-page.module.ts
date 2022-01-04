ea5dfcc3715637125773e9d4abfa5c34795ecbb4
Psycomentis06 <alibenamor30@gmail.com>
2022-01-04 18:48:56 +0100

    LessonPageComponent

--- src/app/home/lessons-page/lessons-page.module.ts
+++ src/app/home/lessons-page/lessons-page.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LessonsPageRoutingModule } from './lessons-page-routing.module';
 import { LessonsPageComponent } from './lessons-page.component';
+import { LessonPageComponent } from './lesson-page/lesson-page.component';
 
 
 
 @NgModule({
   declarations: [
-    LessonsPageComponent
+    LessonsPageComponent,
+    LessonPageComponent
   ],
   imports: [
     CommonModule,
