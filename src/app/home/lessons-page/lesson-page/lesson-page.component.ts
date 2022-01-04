e6d995fa462b7fd9a91ed9170593ef722b95c30f
Psycomentis06 <alibenamor30@gmail.com>
2022-01-04 21:25:20 +0100

    1st

--- src/app/home/lessons-page/lesson-page/lesson-page.component.ts
+++ src/app/home/lessons-page/lesson-page/lesson-page.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CourseService } from 'src/app/services/course.service';
+import { faPaintBrush, faChevronLeft, faUser, faBookmark } from '@fortawesome/free-solid-svg-icons'
+import { ThemeModalService } from 'src/app/core/service/theme-modal.service';
+import { Observable } from 'rxjs';
+import { ICourse } from 'src/app/interfaces/course.interface';
 
 @Component({
   selector: 'app-lesson-page',
   templateUrl: './lesson-page.component.html',
-  styleUrls: ['./lesson-page.component.scss']
+  styleUrls: ['./lesson-page.component.scss'],
 })
 export class LessonPageComponent implements OnInit {
-
-  constructor() { }
+  fontAwesomeIcons = {
+    faPaintBrush,
+    faChevronLeft,
+    faUser,
+    faBookmark
+  }
+  $courseObservable: Observable<ICourse>
+  constructor(
+    private courseService: CourseService,
+    private activatedRoute: ActivatedRoute,
+    public themeModalService: ThemeModalService
+  ) {
+    this.$courseObservable = this.getCourse()
+  }
 
   ngOnInit(): void {
   }
 
+  getCourse(): Observable<ICourse> {
+    return this.courseService
+      .get(Number(this.activatedRoute.snapshot.paramMap.get('id')));
+  }
 }
