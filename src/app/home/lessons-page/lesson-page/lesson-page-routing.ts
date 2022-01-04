a886a6566ee6597524ee10358235951e8c057aa3
Psycomentis06 <alibenamor30@gmail.com>
2022-01-04 22:22:38 +0100

    MetaData

--- src/app/home/lessons-page/lesson-page/lesson-page-routing.ts
+++ src/app/home/lessons-page/lesson-page/lesson-page-routing.ts
@@ -4,25 +4,33 @@ import { LessonChapterComponent } from "./lesson-chapter/lesson-chapter.componen
 import { LessonOverviewComponent } from "./lesson-overview/lesson-overview.component";
 import { LessonPageComponent } from "./lesson-page.component";
 const routes: Routes = [
-    {
-        path: ':id',
-        component: LessonPageComponent,
+  {
+    path: ':id',
+    component: LessonPageComponent,
+    data: {
+      hideHeader: true,
+      hideFooter: false,
+    },
+    children: [
+      {
+        path: '',
+        component: LessonOverviewComponent,
         data: {
-            hideHeader: true,
-            hideFooter: false
+          hideHeader: true,
+          hideFooter: false,
         },
-        children: [
-            {
-                path: '',
-                component: LessonOverviewComponent
-            },
-            {
-                path: 'chapter',
-                component: LessonChapterComponent
-            }
-        ]
-    }
-]
+      },
+      {
+        path: ':chapterId',
+        component: LessonChapterComponent,
+        data: {
+          hideHeader: true,
+          hideFooter: false,
+        },
+      },
+    ],
+  },
+];
 @NgModule({
     imports: [RouterModule.forChild(routes)],
     exports: [RouterModule]
