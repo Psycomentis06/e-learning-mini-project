64f447addaf234602b970a3090fc4d2be8ea49aa
Psycomentis06 <alibenamor30@gmail.com>
2022-01-04 23:17:11 +0100

    Declare components

--- src/app/home/lessons-page/lesson-page/lesson-page.module.ts
+++ src/app/home/lessons-page/lesson-page/lesson-page.module.ts
@@ -5,11 +5,13 @@ import { LessonPageRoutingModule } from './lesson-page-routing';
 import { LessonChapterComponent } from './lesson-chapter/lesson-chapter.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { LessonOverviewComponent } from './lesson-overview/lesson-overview.component';
+import { LessonContentComponent } from './lesson-content/lesson-content.component';
+import { DescriptionFormatterPipe } from './lesson-content/description-formatter.pipe';
 
 
 
 @NgModule({
-  declarations: [LessonPageComponent, LessonChapterComponent, LessonOverviewComponent],
+  declarations: [LessonPageComponent, LessonChapterComponent, LessonOverviewComponent, LessonContentComponent, DescriptionFormatterPipe],
   imports: [
     CommonModule,
     LessonPageRoutingModule,
