b108f41cafb1f9bd346effc89882343baa098de9
Psycomentis06 <alibenamor30@gmail.com>
2022-01-04 20:13:20 +0100

    chapter route

--- src/app/home/lessons-page/lesson-page/lesson-page-routing.ts
+++ src/app/home/lessons-page/lesson-page/lesson-page-routing.ts
@@ -1,14 +1,25 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { LessonChapterComponent } from "./lesson-chapter/lesson-chapter.component";
 import { LessonPageComponent } from "./lesson-page.component";
-const roues: Routes = [
+const routes: Routes = [
     {
         path: ':id',
-        component: LessonPageComponent
+        component: LessonPageComponent,
+        data: {
+            hideHeader: true,
+            hideFooter: false
+        },
+        children: [
+            {
+                path: 'chapter',
+                component: LessonChapterComponent
+            }
+        ]
     }
 ]
 @NgModule({
-    imports: [RouterModule.forChild(roues)],
+    imports: [RouterModule.forChild(routes)],
     exports: [RouterModule]
 })
 export class LessonPageRoutingModule { }
