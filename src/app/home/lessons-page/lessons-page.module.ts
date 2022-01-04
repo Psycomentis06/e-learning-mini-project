bb060a7eab1268a9e3dc5d334ceaacca5ecbc1ee
Psycomentis06 <alibenamor30@gmail.com>
2022-01-04 17:18:55 +0100

    declare comp

--- src/app/home/lessons-page/lessons-page.module.ts
+++ src/app/home/lessons-page/lessons-page.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LessonsPageRoutingModule } from './lessons-page-routing.module';
+import { LessonsPageComponent } from './lessons-page.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LessonsPageComponent
+  ],
   imports: [
     CommonModule,
     LessonsPageRoutingModule
