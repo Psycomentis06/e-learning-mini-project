6954a5f2f41adce5565a771e63e985d253a69ad8
Psycomentis06 <alibenamor30@gmail.com>
2022-01-04 13:34:42 +0100

    GET

---
+++ src/app/services/language.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { ILanguage } from '../interfaces/language.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LanguageService {
+  private API_URL = environment.api.api_1;
+  constructor(private http: HttpClient) { }
+
+  getAll(limit: number = 20):Observable<ILanguage>  {
+    return this.http.get<ILanguage>(this.API_URL + "/languages", {
+      params: {
+        limit
+      }
+    })
+  }
+}
