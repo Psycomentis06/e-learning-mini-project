768de9b420320e33c781bf900e42f741db483111
Psycomentis06 <alibenamor30@gmail.com>
2022-01-04 21:28:21 +0100

    Lesson Overview

--- src/app/home/lessons-page/lesson-page/lesson-page.module.ts
+++ src/app/home/lessons-page/lesson-page/lesson-page.module.ts
@@ -4,11 +4,12 @@ import { LessonPageComponent } from './lesson-page.component';
 import { LessonPageRoutingModule } from './lesson-page-routing';
 import { LessonChapterComponent } from './lesson-chapter/lesson-chapter.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { LessonOverviewComponent } from './lesson-overview/lesson-overview.component';
 
 
 
 @NgModule({
-  declarations: [LessonPageComponent, LessonChapterComponent],
+  declarations: [LessonPageComponent, LessonChapterComponent, LessonOverviewComponent],
   imports: [
     CommonModule,
     LessonPageRoutingModule,
