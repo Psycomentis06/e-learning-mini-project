0feb7af9d553eb70bd478f5e9f020e6effb46c83
Psycomentis06 <alibenamor30@gmail.com>
2022-01-04 17:19:11 +0100

    1st

--- src/app/home/lessons-page/lessons-page.component.ts
+++ src/app/home/lessons-page/lessons-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, debounceTime, distinctUntilChanged, map, Observable, switchMap } from 'rxjs';
+import { ICourse } from 'src/app/interfaces/course.interface';
+import { CourseService } from 'src/app/services/course.service';
 
 @Component({
   selector: 'app-lessons-page',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lessons-page.component.scss']
 })
 export class LessonsPageComponent implements OnInit {
-
-  constructor() { }
+  
+  courses: ICourse[]
+  $filterSubject: BehaviorSubject<string>
+  _filterObservable: Observable<string>
+  constructor(private coursesService: CourseService) {
+    this.$filterSubject = new BehaviorSubject("")
+    this._filterObservable = this.$filterSubject.asObservable()
+    this.courses = []
+  }
 
   ngOnInit(): void {
+    this._filterObservable.pipe(
+      debounceTime(1000),
+      distinctUntilChanged(),
+      switchMap((v) => this.coursesService.getAll(30, v))
+    ).subscribe(d => {
+      this.courses = d
+    })
+  }
+
+  search(value: string) {
+    this.$filterSubject.next(value)
   }
 
 }
