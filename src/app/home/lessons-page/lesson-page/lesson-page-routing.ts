1fb340569453a9fd6a15dcca68c85346af90dc54
Psycomentis06 <alibenamor30@gmail.com>
2022-01-04 23:16:22 +0100

    lesson content route

--- src/app/home/lessons-page/lesson-page/lesson-page-routing.ts
+++ src/app/home/lessons-page/lesson-page/lesson-page-routing.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { LessonChapterComponent } from "./lesson-chapter/lesson-chapter.component";
+import { LessonContentComponent } from "./lesson-content/lesson-content.component";
 import { LessonOverviewComponent } from "./lesson-overview/lesson-overview.component";
 import { LessonPageComponent } from "./lesson-page.component";
 const routes: Routes = [
@@ -23,6 +24,14 @@ const routes: Routes = [
       {
         path: ':chapterId',
         component: LessonChapterComponent,
+        data: {
+          hideHeader: true,
+          hideFooter: false,
+        },
+      },
+      {
+        path: ':chapterId/:lessonId',
+        component: LessonContentComponent,
         data: {
           hideHeader: true,
           hideFooter: false,
