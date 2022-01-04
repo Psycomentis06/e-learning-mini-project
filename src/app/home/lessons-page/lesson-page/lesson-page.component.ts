da66ee8d28172f1a6c1ea7d03eae330dd65a835e
Psycomentis06 <alibenamor30@gmail.com>
2022-01-04 18:48:40 +0100

    cli generated

---
+++ src/app/home/lessons-page/lesson-page/lesson-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lesson-page',
+  templateUrl: './lesson-page.component.html',
+  styleUrls: ['./lesson-page.component.scss']
+})
+export class LessonPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
